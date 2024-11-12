7d016921e54a606955f9258182f8cfe1f75173f5
TillWege <tillwege@gmail.com>
2024-11-12 21:18:18 +0100

    add basic background

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,35 @@
 #include "imgui.h"
 #include "rlImGui.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+#define SCREEN_WIDTH (1920)
+#define SCREEN_HEIGHT (1080)
 
-#define WINDOW_TITLE "Window title"
+#define WINDOW_TITLE "PokeOS"
+#define SQUARE_SIZE 50
+
+Texture texture;
+
+void loadAssets()
+{
+	texture = LoadTexture(ASSETS_PATH"bg.png");
+}
+
+void draw_background()
+{
+	static int scale = 5;
+
+	ImGui::Begin("Background");
+	ImGui::SliderInt("Scale", &scale, 1, 10);
+	ImGui::End();
+
+
+	Vector2 position = {
+		SCREEN_WIDTH / 2 - texture.width * float(scale) / 2,
+		SCREEN_HEIGHT / 2 - texture.height * float(scale) / 2
+	};
+	DrawTextureEx(texture, position, 1, scale, WHITE);
+
+}
 
 int main(void)
 {
@@ -13,16 +38,17 @@ int main(void)
 	SetTargetFPS(60);
 	rlImGuiSetup(true);
 
+	loadAssets();
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		rlImGuiBegin();
 
-		ClearBackground(RAYWHITE);
+		ClearBackground(Color{ 0x38, 0x3c, 0x7d, 0xFF });
+		draw_background();
 
-		const char* text = "OMG! IT WORKS!";
-		const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-		DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, SCREEN_HEIGHT / 2, 20, BLACK);
+
 
 		bool open = true;
 		ImGui::ShowDemoWindow(&open);
