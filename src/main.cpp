6525b6872566b01d0bc48064c74bd484200ba354
TillWege <tillwege@gmail.com>
2024-11-22 23:45:18 +0100

    Add basic Sqlite functionality to app via SQLiteCPP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include "imgui.h"
 #include "rlImGui.h"
+#include "SQLiteCpp/Database.h"
+#include "SQLiteCpp/Statement.h"
 
 #define SCREEN_WIDTH (1920)
 #define SCREEN_HEIGHT (1080)
@@ -10,30 +12,38 @@
 
 Texture texture;
 
+void loadDatabase()
+{
+	auto db = SQLite::Database(ASSETS_PATH "pokemon.db");
+
+	auto query = SQLite::Statement(db, "SELECT COUNT(*) FROM pokemon");
+	query.executeStep();
+	int count = query.getColumn(0);
+	printf("Count: %d\n", count);
+}
+
 void loadAssets()
 {
-	texture = LoadTexture(ASSETS_PATH"bg.png");
+	texture = LoadTexture(ASSETS_PATH "bg.png");
 }
 
 void draw_background()
 {
-	static int scale = 5;
+	static float scale = 5;
 
 	ImGui::Begin("Background");
-	ImGui::SliderInt("Scale", &scale, 1, 10);
+	ImGui::DragFloat("Scale", &scale, 1, 1.0, 10.0);
 	ImGui::End();
-
 
 	Vector2 position = {
 		SCREEN_WIDTH / 2 - texture.width * float(scale) / 2,
-		SCREEN_HEIGHT / 2 - texture.height * float(scale) / 2
-	};
+		SCREEN_HEIGHT / 2 - texture.height * float(scale) / 2};
 	DrawTextureEx(texture, position, 1, scale, WHITE);
-
 }
 
-int main(void)
+int main()
 {
+	loadDatabase();
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
 	SetTargetFPS(60);
 	rlImGuiSetup(true);
@@ -45,10 +55,8 @@ int main(void)
 		BeginDrawing();
 		rlImGuiBegin();
 
-		ClearBackground(Color{ 0x38, 0x3c, 0x7d, 0xFF });
+		ClearBackground(Color{0x38, 0x3c, 0x7d, 0xFF});
 		draw_background();
-
-
 
 		bool open = true;
 		ImGui::ShowDemoWindow(&open);
